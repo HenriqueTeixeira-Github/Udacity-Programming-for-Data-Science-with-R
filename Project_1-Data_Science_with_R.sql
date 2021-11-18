dfbb67abadfac9463b4baa45d0faa7ec303c8c61
Henrique Teixeira <henriquetcro.tech@gmail.com>
2021-11-17 21:21:39 -0300

    Update Project_1-Data_Science_with_R.sql

--- Project_1-Data_Science_with_R.sql
+++ Project_1-Data_Science_with_R.sql
@@ -30,3 +30,30 @@ JOIN rental AS r
 ON i.inventory_id = r.inventory_id
 GROUP BY 1,2
 ORDER BY 2,1;
+
+-- Question #2
+
+-- Now we need to know how the length of rental duration of these family-friendly movies compares to the duration that all movies are rented for.
+-- Can you provide a table with the movie titles and divide them into 4 levels (first_quarter, second_quarter, third_quarter, and final_quarter)
+-- based on the quartiles (25%, 50%, 75%) of the rental duration for movies across all categories? Make sure to also indicate the category that these family-friendly movies fall into.
+
+WITH family_films AS (
+    SELECT
+        f.film_id AS film_id,
+        f.title AS film_title,
+        c.name AS category_name,
+        f.rental_duration AS rental_duration
+    FROM film AS f
+    JOIN film_category AS fc
+    ON f.film_id = fc.film_id
+    JOIN category AS c
+    ON c.category_id = fc.category_id
+    WHERE c.name IN ('Animation', 'Children','Classics','Comedy','Family','Music')
+)
+
+SELECT
+    film_title,
+    category_name,
+    rental_duration,
+	NTILE(4) OVER (ORDER BY rental_duration) AS quartile
+FROM family_films AS ff
